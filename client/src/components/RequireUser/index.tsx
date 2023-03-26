7ccf6eca02b19e6c2924c7f5fcd3d60eccf9701e
Ciprian Platica <platica.ciprian@gmail.com>
2023-03-27 00:44:48 +0300

    Fix redirect on logout

--- client/src/components/RequireUser/index.tsx
+++ client/src/components/RequireUser/index.tsx
@@ -1,16 +1,18 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { userApi } from "@/redux/api/userApi";
 import FullScreenLoader from "@/components/FullScreenLoader";
+import { useAppSelector } from "@/redux/store";
 
 const RequireUser = () => {
   const location = useLocation();
   const { isSuccess, isLoading } = userApi.endpoints.getMe.useQueryState(null);
+  const user = useAppSelector((state) => state.userState.user);
 
   if (isLoading) {
     return <FullScreenLoader />;
   }
 
-  return isSuccess ? (
+  return isSuccess && user ? (
     <Outlet />
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
