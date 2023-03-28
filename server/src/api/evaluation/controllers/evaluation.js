4a92763b536d369acde1253e9e622997476b21be
Ciprian Platica <platica.ciprian@gmail.com>
2023-03-29 00:53:00 +0300

    Send emails after finishing evaluation

--- server/src/api/evaluation/controllers/evaluation.js
+++ server/src/api/evaluation/controllers/evaluation.js
@@ -14,10 +14,70 @@ module.exports = createCoreController(
       const urlParams = new URLSearchParams(ctx.request.url.split("?")[1]);
       const response = await super.findOne(ctx);
       if (response?.data?.attributes?.email !== urlParams.get("email")) {
-        throw new UnauthorizedError('Sorry, you are not authorized to access this page.');
+        throw new UnauthorizedError(
+          "Sorry, you are not authorized to access this page."
+        );
       } else {
         return response;
       }
+    },
+    async update(ctx) {
+      const response = await super.update(ctx);
+      const { dimensions } = ctx.request.body.data;
+      const matrix = await strapi.entityService.findMany("api::matrix.matrix", {
+        populate: "dimensions.quiz",
+      });
+      if (dimensions?.length === matrix?.dimensions?.length) {
+        const mailText = matrix?.dimensions.reduce(
+          (acc, { name, quiz }, dimensionIndex) => {
+            return `${acc}\nDimensiune: ${name}${quiz.reduce(
+              (acc, matrixQuiz, quizIndex) =>
+                `${acc}\nIntrebare: ${matrixQuiz.question}\nRaspuns: ${
+                  matrixQuiz[
+                    `option_${
+                      dimensions[dimensionIndex]?.quiz[quizIndex]?.answer + 1
+                    }`
+                  ]
+                }`,
+              ""
+            )}\nComentariu: ${dimensions[dimensionIndex]?.comment}`;
+          },
+          ""
+        );
+        const mailHtml = matrix?.dimensions.reduce(
+          (acc, { name, quiz }, dimensionIndex) => {
+            return `${acc}\n<h2>${name}</h2>${quiz.reduce(
+              (acc, matrixQuiz, quizIndex) =>
+                `${acc}<h3>Intrebare: ${matrixQuiz.question}</h3><p>Raspuns: ${
+                  matrixQuiz[
+                    `option_${
+                      dimensions[dimensionIndex]?.quiz[quizIndex]?.answer + 1
+                    }`
+                  ]
+                }</p>`,
+              ""
+            )}${
+              dimensions[dimensionIndex]?.comment &&
+              `<p>Comentariu: ${dimensions[dimensionIndex]?.comment}</p>`
+            }`;
+          },
+          ""
+        );
+        try {
+          const email = response?.data?.attributes?.email;
+          if (email)
+            await strapi.plugins["email"].services.email.send({
+              to: email,
+              subject:
+                "CRESTEM-ONG: Raspunsurile dvs pentru formularul de evaluare a organizatiei",
+              text: mailText,
+              html: mailHtml,
+            });
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      return response;
     },
   })
 );
