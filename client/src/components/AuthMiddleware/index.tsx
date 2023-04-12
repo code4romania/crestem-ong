3e5552759cadceb1a3097729dcbfc6aaebf6d8dc
Ciprian Platica <platica.ciprian@gmail.com>
2023-04-13 02:14:35 +0300

    Remove cookie if user failed

--- client/src/components/AuthMiddleware/index.tsx
+++ client/src/components/AuthMiddleware/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import FullScreenLoader from "@/components/FullScreenLoader";
 import { userApi } from "@/redux/api/userApi";
 import { useAppSelector } from "@/redux/store";
+import Cookies from "js-cookie";
 
 type IAuthMiddleware = {
   children: React.ReactElement;
@@ -10,10 +11,16 @@ type IAuthMiddleware = {
 const AuthMiddleware: React.FC<IAuthMiddleware> = ({ children }) => {
   const token = useAppSelector((state) => state.userState.token);
   const user = useAppSelector((state) => state.userState.user);
-  const { isLoading } = userApi.endpoints.getMe.useQuery(null, {
+  const { isLoading, isError } = userApi.endpoints.getMe.useQuery(null, {
     skip: !token,
     refetchOnMountOrArgChange: true,
   });
+
+  useEffect(() => {
+    if (isError) {
+      Cookies.remove("jwt");
+    }
+  }, [isError]);
 
   if ((token && !user) || isLoading) {
     return (
