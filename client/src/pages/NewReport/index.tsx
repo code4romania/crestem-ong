9ce402faddb5ff011b7daac59026c065b5fb4016
Ciprian Platica <platica.ciprian@gmail.com>
2023-05-15 21:40:01 +0300

    Scroll to top on new report

--- client/src/pages/NewReport/index.tsx
+++ client/src/pages/NewReport/index.tsx
@@ -64,6 +64,10 @@ const NewReport = () => {
     });
   }, []);
 
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
   const handleTextareaChange = (e) => {
     const value = e.target.value.replace(/ /g, "");
     e.target.value = value;
