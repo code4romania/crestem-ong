62d1978cac253f360c66768a3ced9df5c20297fe
Ciprian Platica <platica.ciprian@gmail.com>
2023-08-09 00:34:05 +0300

    Add filters to Users table

--- client/src/pages/Home/UsersTable.tsx
+++ client/src/pages/Home/UsersTable.tsx
@@ -5,18 +5,32 @@ import React, {
   useRef,
   useState,
 } from "react";
-import { useGetUsersQuery } from "@/redux/api/userApi";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { downloadExcel } from "react-export-table-to-excel";
 import Input from "@/components/Input";
 import { User } from "@/redux/api/types";
-import { downloadExcel } from "react-export-table-to-excel";
+import { useGetUsersQuery } from "@/redux/api/userApi";
 import Button from "@/components/Button";
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod";
+import { object, string, TypeOf } from "zod";
+
+const filtersSchema = object({
+  search: string(),
+  startDate: string(),
+  endDate: string(),
+});
+export type FiltersInput = TypeOf<typeof filtersSchema>;
 
 const UsersTable = () => {
   const tableRef = useRef(null);
   const { data } = useGetUsersQuery(null);
-  const [filtered, setFilterd] = useState([] as User[]);
+  const [filtered, setFilterd] = useState([]);
+
   const [searchTerm, setSearchTerm] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
   const header = [
     "NUME ONG",
     "DATĂ ÎNREGISTRARE",
@@ -43,10 +57,15 @@ const UsersTable = () => {
         )
       : "-",
   }));
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeSearch = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
-
+  const handleChangeStartDate = (event: ChangeEvent<HTMLInputElement>) => {
+    setStartDate(event.target.value);
+  };
+  const handleChangeEndDate = (event: ChangeEvent<HTMLInputElement>) => {
+    setEndDate(event.target.value);
+  };
   const handleDownloadExcel = useCallback(() => {
     downloadExcel({
       fileName: "organizații",
@@ -59,13 +78,19 @@ const UsersTable = () => {
   }, [header, body]);
 
   useEffect(() => {
-    const results = data?.filter((res) =>
-      res.ongName.toLowerCase().includes(searchTerm)
+    const results = data?.filter(
+      (res) =>
+        res.ongName.toLowerCase().includes(searchTerm) &&
+        (startDate ? new Date(res.createdAt) > new Date(startDate) : true) &&
+        (endDate && res.reports?.length > 0
+          ? new Date(res.reports[res.reports?.length - 1].createdAt) >
+            new Date(endDate)
+          : true)
     );
     if (results) {
       setFilterd(results);
     }
-  }, [data, searchTerm]);
+  }, [data, searchTerm, startDate, endDate]);
 
   if (!data) {
     return <></>;
@@ -73,17 +98,36 @@ const UsersTable = () => {
 
   return (
     <>
-      <div className="flex items-center justify-between mb-10">
-        <div className="w-2/3">
-          <Input
-            name="search"
-            placeholder="Caută"
-            onChange={handleChange}
-            value={searchTerm}
-          />
+      <form className="flex items-end justify-between mb-10">
+        <div className="flex items-center justify-between space-x-2.5">
+          <div className="w-2/3">
+            <label>Caută</label>
+            <Input
+              type="search"
+              name="search"
+              placeholder="Caută"
+              onChange={handleChangeSearch}
+            />
+          </div>
+          <div>
+            <label>Dată înregistrare</label>
+            <Input
+              name="startDate"
+              type="date"
+              onChange={handleChangeStartDate}
+            />
+          </div>
+          <div>
+            <label>Ultima evaluare</label>
+            <Input name="endDate" type="date" onChange={handleChangeEndDate} />
+          </div>
         </div>
-        <Button onClick={handleDownloadExcel}>Descarcă tabel</Button>
-      </div>
+        <div>
+          <Button type="button" onClick={handleDownloadExcel}>
+            Descarcă tabel
+          </Button>
+        </div>
+      </form>
       <table
         ref={tableRef}
         className="w-full table-auto divide-y divide-gray-300"
