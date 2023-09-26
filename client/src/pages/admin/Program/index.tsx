3a944588a82359981cdddb9259189802a339c4cb
Ciprian Platica <platica.ciprian@gmail.com>
2023-09-27 02:07:33 +0300

    Add empty screens for programs tables

--- client/src/pages/admin/Program/index.tsx
+++ client/src/pages/admin/Program/index.tsx
@@ -13,6 +13,7 @@ import Table from "@/components/Table";
 import Button from "@/components/Button";
 import AddUserInProgram from "@/components/AddUserInProgram";
 import AddMentorInProgram from "@/components/AddMentorInProgram";
+import EmptyScreen from "@/components/EmptyScreen";
 
 const Program = () => {
   const { programId = "" } = useParams();
@@ -95,99 +96,109 @@ const Program = () => {
           ]}
         />
       </Section>
-      {data?.users?.length > 0 && (
-        <Section>
-          <Table
-            title="ONG-uri în program"
-            button={
-              availableUsers?.length > 0 && (
-                <>
-                  <Button onClick={() => setOpenUserInProgram(true)}>
-                    Adaugă ONG
-                  </Button>
-                  <AddUserInProgram
-                    open={openUserInProgram}
-                    users={availableUsers}
-                    setOpen={setOpenUserInProgram}
-                    onSave={handleOnAddUserInProgram}
-                  />
-                </>
-              )
-            }
-            head={[
-              "Nume ONG",
-              "Nume reprezentant",
-              "Data intrare program",
-              "Ultima evaluare",
-              "",
-            ]}
-            body={data.users.map((user) => [
-              user.ongName,
-              `${user.contactFirstName} ${user.contactLastName}`,
-              "-",
-              `${
-                user.reports?.[0]
-                  ? new Date(user.reports?.[0].createdAt).toLocaleString(
-                      "ro-RO",
-                      {
-                        month: "short",
-                        day: "numeric",
-                        year: "numeric",
-                      }
-                    )
-                  : "-"
-              }`,
-              <NavLink to={`/users/${user.id}`}>Vezi</NavLink>,
-            ])}
-          />
-        </Section>
-      )}
-      {data?.mentors?.length > 0 && (
-        <Section>
-          <Table
-            title="Persoane resursă în program"
-            button={
-              availableMentors?.length > 0 && (
-                <>
-                  <Button onClick={() => setOpenMentorInProgram(true)}>
-                    Adaugă persoană resursă
-                  </Button>
-                  <AddMentorInProgram
-                    open={openMentorInProgram}
-                    mentors={availableMentors}
-                    setOpen={setOpenMentorInProgram}
-                    onSave={handleOnAddUMentorInProgram}
-                  />
-                </>
-              )
-            }
-            head={[
+      (
+      <Section>
+        <Table
+          title="ONG-uri în program"
+          button={
+            availableUsers?.length > 0 && (
+              <>
+                <Button onClick={() => setOpenUserInProgram(true)}>
+                  Adaugă ONG
+                </Button>
+                <AddUserInProgram
+                  open={openUserInProgram}
+                  users={availableUsers}
+                  setOpen={setOpenUserInProgram}
+                  onSave={handleOnAddUserInProgram}
+                />
+              </>
+            )
+          }
+          head={
+            data.users?.length > 0
+              ? [
+                  "Nume ONG",
+                  "Nume reprezentant",
+                  "Data intrare program",
+                  "Ultima evaluare",
+                  "",
+                ]
+              : []
+          }
+          body={data.users.map((user) => [
+            user.ongName,
+            `${user.contactFirstName} ${user.contactLastName}`,
+            "-",
+            `${
+              user.reports?.[0]
+                ? new Date(user.reports?.[0].createdAt).toLocaleString(
+                    "ro-RO",
+                    {
+                      month: "short",
+                      day: "numeric",
+                      year: "numeric",
+                    }
+                  )
+                : "-"
+            }`,
+            <NavLink to={`/users/${user.id}`}>Vezi</NavLink>,
+          ])}
+        />
+        {data?.users?.length === 0 && (
+          <EmptyScreen title={"Nicio organizatie inscrisa in program"} />
+        )}
+      </Section>
+      )
+      <Section>
+        <Table
+          title="Persoane resursă în program"
+          button={
+            availableMentors?.length > 0 && (
+              <>
+                <Button onClick={() => setOpenMentorInProgram(true)}>
+                  Adaugă persoană resursă
+                </Button>
+                <AddMentorInProgram
+                  open={openMentorInProgram}
+                  mentors={availableMentors}
+                  setOpen={setOpenMentorInProgram}
+                  onSave={handleOnAddUMentorInProgram}
+                />
+              </>
+            )
+          }
+          head={
+            data?.mentors?.length > 0 && [
               "Nume",
               "Specializare",
               "Disponibilitate",
               "Ultima activitate",
               "",
-            ]}
-            body={data.mentors.map((mentor) => [
-              `${mentor.firstName} ${mentor.lastName}`,
-              mentor.dimensions?.map((dimension) => dimension.name).join(", "),
-              `${mentor.available ? "Disponibil" : "Indisponibil"}`,
-              `${
-                mentor.mentorActivities?.length
-                  ? new Date(
-                      mentor.mentorActivities[0]?.createdAt
-                    )?.toLocaleString("ro-RO", {
-                      month: "short",
-                      day: "numeric",
-                      year: "numeric",
-                    })
-                  : "-"
-              }`,
-              <NavLink to={`/users/${mentor.id}`}>Vezi</NavLink>,
-            ])}
-          />
-        </Section>
-      )}
+            ]
+          }
+          body={data.mentors.map((mentor) => [
+            `${mentor.firstName} ${mentor.lastName}`,
+            mentor.dimensions?.map((dimension) => dimension.name).join(", "),
+            `${mentor.available ? "Disponibil" : "Indisponibil"}`,
+            `${
+              mentor.mentorActivities?.length
+                ? new Date(
+                    mentor.mentorActivities[0]?.createdAt
+                  )?.toLocaleString("ro-RO", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                  })
+                : "-"
+            }`,
+            <NavLink to={`/users/${mentor.id}`}>Vezi</NavLink>,
+          ])}
+        />
+        {data?.mentors?.length === 0 && (
+          <EmptyScreen title={"Nicio persoana resursa inscrisa in program"} />
+        )}
+      </Section>
     </>
   );
 };
