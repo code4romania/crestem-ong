7d8b9cc5efb5cd5256c49bf145f76b981a8c316f
Ciprian Platica <platica.ciprian@gmail.com>
2023-11-12 23:35:50 +0200

    Use avatar image instead of random

--- client/src/components/UserMenu/index.tsx
+++ client/src/components/UserMenu/index.tsx
@@ -58,11 +58,28 @@ const UserMenu = () => {
         <span>{user.ongName}</span>
         <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
           <span className="sr-only">Open user menu</span>
-          <img
-            className="h-8 w-8 rounded-full"
-            src={`https://i.pravatar.cc/150?u=${user?.email}`}
-            alt="avatar"
-          />
+          {user?.avatar ? (
+            <img
+              className="h-8 w-8 rounded-full"
+              src={user?.avatar?.url}
+              alt="avatar"
+            />
+          ) : (
+            <div className="relative h-8 w-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+              <svg
+                className="absolute w-6 h-6 text-gray-400 left-1 top-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </div>
+          )}
         </Menu.Button>
       </div>
       <Transition
