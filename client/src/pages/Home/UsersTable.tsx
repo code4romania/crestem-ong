ea329a0b4606145d2c3bbda9e99ecd04b2605f91
Ion Dormenco <idormenco@gmail.com>
2023-11-29 18:15:07 +0200

    Update UsersTable.tsx
    closes #85

--- client/src/pages/Home/UsersTable.tsx
+++ client/src/pages/Home/UsersTable.tsx
@@ -1,16 +1,18 @@
-import React, {
+import {
   ChangeEvent,
   useCallback,
   useEffect,
+  useMemo,
   useRef,
   useState,
 } from "react";
 import { Link } from "react-router-dom";
 import { downloadExcel } from "react-export-table-to-excel";
 import Input from "@/components/Input";
-import { useGetUsersQuery } from "@/redux/api/userApi";
+import { useGetDomainsQuery, useGetUsersQuery } from "@/redux/api/userApi";
 import Button from "@/components/Button";
 import { object, string, TypeOf } from "zod";
+import citiesByCounty from "@/lib/orase-dupa-judet.json";
 
 const filtersSchema = object({
   search: string(),
@@ -20,6 +22,13 @@ const filtersSchema = object({
 export type FiltersInput = TypeOf<typeof filtersSchema>;
 
 const UsersTable = () => {
+  const counties = [...Object.keys(citiesByCounty)
+    .sort()
+    .map((county: string) => ({
+      label: county,
+      name: county,
+    }))];
+
   const tableRef = useRef(null);
   const { data } = useGetUsersQuery(null);
   const [filtered, setFilterd] = useState([]);
@@ -27,15 +36,40 @@ const UsersTable = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [county, setCounty] = useState("");
+  const [locality, setLocality] = useState("");
+  const [domain, setDomain] = useState(-1);
+
+  const { data: domains } = useGetDomainsQuery(null);
+
+  const localities = useMemo(
+    () =>
+      county
+        ? [
+          ...citiesByCounty[county].map((city) => city.nume)
+            .filter((value, index, array) => array.indexOf(value) === index)
+            .sort()
+            .map((city) => ({
+              name: city,
+              label: city,
+            }))
+        ]
+        : [],
+    [citiesByCounty, county]
+  );
+
 
   const header = [
     "NUME ONG",
     "DATĂ ÎNREGISTRARE",
     "PROGRAM",
+    "JUDEȚ",
+    "LOCALITATE",
+    "DOMENIU DE ACTIVITATE",
     "ULTIMA EVALUARE",
   ];
 
-  const body = filtered.map(({ ongName, createdAt, reports, program }) => ({
+  const body = filtered.map(({ ongName, createdAt, reports, program, county, city, domains }) => ({
     ongName,
     createdAt: new Date(createdAt).toLocaleString("ro-RO", {
       month: "short",
@@ -43,17 +77,21 @@ const UsersTable = () => {
       year: "numeric",
     }),
     program: program || "-",
+    county,
+    city,
+    domains: domains?.length ? domains.map(d => d.name).join(",") : "-",
     lastEvaluationDate: reports?.length
       ? new Date(reports[reports?.length - 1].createdAt).toLocaleString(
-          "ro-RO",
-          {
-            month: "short",
-            day: "numeric",
-            year: "numeric",
-          }
-        )
+        "ro-RO",
+        {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+        }
+      )
       : "-",
   }));
+
   const handleChangeSearch = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
@@ -62,6 +100,24 @@ const UsersTable = () => {
   };
   const handleChangeEndDate = (event: ChangeEvent<HTMLInputElement>) => {
     setEndDate(event.target.value);
+  };
+  const handleChangeDomain = (event: ChangeEvent<HTMLSelectElement>) => {
+    setDomain(+event.target.value);
+  };
+  const handleChangeCounty = (event: ChangeEvent<HTMLSelectElement>) => {
+    setCounty(event.target.value);
+  };
+  const handleChangeLocality = (event: ChangeEvent<HTMLSelectElement>) => {
+    setLocality(event.target.value);
+  };
+
+  const handleClearFilters = () => {
+    setSearchTerm("");
+    setStartDate("");
+    setEndDate("");
+    setDomain(-1);
+    setCounty("");
+    setLocality("");
   };
   const handleDownloadExcel = useCallback(() => {
     downloadExcel({
@@ -81,13 +137,16 @@ const UsersTable = () => {
         (startDate ? new Date(res.createdAt) > new Date(startDate) : true) &&
         (endDate && res.reports?.length > 0
           ? new Date(res.reports[res.reports?.length - 1].createdAt) >
-            new Date(endDate)
-          : true)
+          new Date(endDate)
+          : true) &&
+        (!county || res.county === county) &&
+        (!locality || res.city === locality) &&
+        (domain === -1 || res.domains?.some(d => d.id === domain))
     );
     if (results) {
       setFilterd(results);
     }
-  }, [data, searchTerm, startDate, endDate]);
+  }, [data, searchTerm, startDate, endDate, county, locality, domain]);
 
   if (!data) {
     return <></>;
@@ -104,6 +163,7 @@ const UsersTable = () => {
               name="search"
               placeholder="Caută"
               onChange={handleChangeSearch}
+              value={searchTerm}
             />
           </div>
           <div>
@@ -112,11 +172,81 @@ const UsersTable = () => {
               name="startDate"
               type="date"
               onChange={handleChangeStartDate}
+              value={startDate}
             />
           </div>
           <div>
             <label>Ultima evaluare</label>
-            <Input name="endDate" type="date" onChange={handleChangeEndDate} />
+            <Input
+              name="endDate"
+              type="date"
+              onChange={handleChangeEndDate}
+              value={endDate} />
+          </div>
+          {domains?.length && <div>
+            <label>Domenii activitate</label>
+            <div>
+              <select
+                id={"domains"}
+                name={"domains"}
+                title={"Domenii activitate"}
+                className="relative mt-2 rounded-md shadow-sm block border-gray-300 focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
+                value={domain}
+                onChange={handleChangeDomain}
+              >
+                <option key={-1} value={-1}></option>
+                {domains.map((option) => (
+                  <option key={option.id} value={option.id}>
+                    {option.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>}
+          <div>
+            <label>Județ</label>
+            <div>
+              <select
+                id={"județ"}
+                name={"județ"}
+                title={"Județ"}
+                className="relative mt-2 rounded-md shadow-sm block border-gray-300 focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
+                value={county}
+                onChange={handleChangeCounty}
+              >
+                <option key={""} value={""}></option>
+                {counties.map((option) => (
+                  <option key={option.name} value={option.name}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div>
+            <label>Localitate</label>
+            <div>
+              <select
+                id={"localitate"}
+                name={"localitate"}
+                title={"Localitate"}
+                className="relative min-w-[150px] mt-2 rounded-md shadow-sm block border-gray-300 focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
+                onChange={handleChangeLocality}
+                disabled={!county}
+              >
+                <option key={""} value={""}></option>
+                {localities.map((option) => (
+                  <option key={option.name} value={option.name}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="self-end">
+            <Button type="button" color="white" onClick={handleClearFilters}>
+              Resetează
+            </Button>
           </div>
         </div>
         <div className="flex space-x-4">
@@ -149,7 +279,7 @@ const UsersTable = () => {
         </thead>
         <tbody className="divide-y divide-gray-200 bg-white">
           {body.map(
-            ({ ongName, createdAt, program, lastEvaluationDate }, index) => (
+            ({ ongName, createdAt, program, county, city, domains, lastEvaluationDate }, index) => (
               <tr key={filtered[index]?.id}>
                 <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                   {ongName}
@@ -159,6 +289,15 @@ const UsersTable = () => {
                 </td>
                 <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                   {program.name || "-"}
+                </td>
+                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                  {county}
+                </td>
+                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                  {city}
+                </td>
+                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                  {domains}
                 </td>
                 <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                   {lastEvaluationDate}
