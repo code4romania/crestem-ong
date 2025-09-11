ea67bebcd8185c9ca631e7b74d85157913db116c
Ion Dormenco <idormenco@gmail.com>
2025-09-11 15:28:17 +0300

    more cleanup

--- server2/src/extensions/users-permissions/strapi-server.ts
+++ server2/src/extensions/users-permissions/strapi-server.ts
@@ -1,53 +1,61 @@
 import { pick } from "remeda";
-import * as magicLink from "./services/magic-link";
-import customRoutes from "./routes/user";
 
-const findRegistrationInfo = async (ctx) => {
-  const { registrationToken } = ctx.query;
-  if (!registrationToken) {
-    return ctx.badRequest("No registrationToken");
-  }
-  let user = await strapi
-    .query("plugin::users-permissions.user")
-    .findOne({ where: { registrationToken } });
+module.exports = (plugin) => {
+  // Override auth controller
+  const originalAuhtControllerFactory = plugin.controllers.auth;
+  plugin.controllers.auth = ({ strapi }) => {
+    const controller = originalAuhtControllerFactory({ strapi });
 
-  if (!user) {
-    return undefined;
-  }
-  return pick(user, ["id", "email", "ongName"]);
-};
+    controller.findRegistrationInfo = async (ctx) => {
+      const { registrationToken } = ctx.query;
+      if (!registrationToken) {
+        return ctx.badRequest("No registrationToken");
+      }
+      let user = await strapi
+        .query("plugin::users-permissions.user")
+        .findOne({ where: { registrationToken } });
 
-const registerWithConfirmation = async (ctx) => {
-  const { data } = ctx.request.body;
-  const id = data?.id;
-  if (!id) {
-    return ctx.badRequest("Missing user id");
-  }
-  const user = await strapi.entityService.update(
-    "plugin::users-permissions.user",
-    id,
+      if (!user) {
+        return undefined;
+      }
+      return pick(user, ["id", "email", "ongName"]);
+    };
+
+    controller.registerWithConfirmation = async (ctx) => {
+      const { data } = ctx.request.body;
+      const id = data?.id;
+      if (!id) {
+        return ctx.badRequest("Missing user id");
+      }
+      const user = await strapi.entityService.update(
+        "plugin::users-permissions.user",
+        id,
+        {
+          data: { password: data.password, registrationToken: null },
+        }
+      );
+      if (!user) {
+        return ctx.badRequest("Failed to register");
+      }
+      return user;
+    };
+
+    return controller;
+  };
+
+  // Add the new route for reset password
+  plugin.routes["content-api"].routes.push(
     {
-      data: { password: data.password, registrationToken: null },
+      method: "GET",
+      path: "/registration-info",
+      handler: "auth.findRegistrationInfo",
+    },
+    {
+      method: "POST",
+      path: "/register",
+      handler: "auth.registerWithConfirmation",
     }
   );
-  if (!user) {
-    return ctx.badRequest("Failed to register");
-  }
-  return user;
-};
 
-export default (plugin) => {
-  plugin.controllers.auth.findRegistrationInfo = findRegistrationInfo;
-  plugin.controllers.auth.registerWithConfirmation = registerWithConfirmation;
-
-  const userServices = plugin.services.user;
-  plugin.services.user = (params) => {
-    const services = userServices(params);
-    return {
-      ...services,
-      magicLink,
-    };
-  };
-  plugin.routes["content-api"].routes.push(...customRoutes);
   return plugin;
 };
