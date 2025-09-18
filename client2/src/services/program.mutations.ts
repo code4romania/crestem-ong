f6d07d57ffcf700fb7ab6e0467a0cbec10d43f75
Ion Dormenco <idormenco@gmail.com>
2025-09-18 17:08:12 +0300

    fix create program

--- client2/src/services/program.mutations.ts
+++ client2/src/services/program.mutations.ts
@@ -1,11 +1,23 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateProgram } from "./api/update-program.api";
+import { createProgram } from "./api/create-program.api";
 
 export function useUpdateProgramMutation() {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: updateProgram,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["programs"] });
+    },
+  });
+}
+
+export function useCreateProgramMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createProgram,
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: ["programs"] });
     },
