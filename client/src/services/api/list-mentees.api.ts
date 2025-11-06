150011a7b9b634ba0f49a8d4850e51f1b754b43e
Ion Dormenco <idormenco@gmail.com>
2025-11-06 20:24:33 +0200

    Update list-mentees.api.ts

--- client/src/services/api/list-mentees.api.ts
+++ client/src/services/api/list-mentees.api.ts
@@ -1,85 +1,6 @@
 import qs from "qs";
 import { API } from "../api";
-import type {
-  FinalDetailedUserModel,
-  FinalDimensionModel,
-  FinalDomainModel,
-  FinalReportModel,
-  FinalUserModel,
-  RoleModel,
-} from "./types";
-
-interface ListMenteesResponse {
-  data: MenteeModel[];
-  meta: Meta;
-}
-
-interface MenteeModel {
-  id: number;
-  attributes: MenteeAttributes;
-}
-
-interface MenteeAttributes {
-  createdAt: string;
-  updatedAt: string;
-  user: User;
-}
-
-interface User {
-  data: UserModel;
-}
-
-interface UserModel {
-  id: number;
-  attributes: UserAttributes;
-}
-
-interface UserAttributes {
-  username: string;
-  email: string;
-  provider: string;
-  confirmed: boolean;
-  blocked: boolean;
-  ongName: string;
-  ongIdentificationNumber: string;
-  county: string;
-  city: string;
-  phone: string;
-  website: string;
-  keywords: string;
-  description: string;
-  contactFirstName: string;
-  contactLastName: string;
-  contactEmail: string;
-  contactPhone: string;
-  createdAt: string;
-  updatedAt: string;
-  accountFacebook: string;
-  accountTwitter: string;
-  accountTiktok: string;
-  accountInstagram: string;
-  accountLinkedin: string;
-  bio: string;
-  expertise: string;
-  firstName: string;
-  lastName: string;
-  available: boolean;
-  domains: FinalDomainModel[];
-  dimensions: FinalDimensionModel[];
-  reports: FinalReportModel[];
-  role: RoleModel;
-}
-
-interface Meta {
-  pagination: Pagination;
-}
-
-interface Pagination {
-  page: number;
-  pageSize: number;
-  pageCount: number;
-  total: number;
-}
+import type { FinalDetailedUserModel } from "./types";
 
 export const listMentees = (
   userId: number
@@ -99,22 +20,12 @@ export const listMentees = (
     },
   };
 
-  return API.get<ListMenteesResponse>(`api/mentorship-requests`, {
+  return API.get<Array<FinalDetailedUserModel>>(`api/mentorship-requests`, {
     params,
     paramsSerializer: {
       serialize: (params) => {
         return qs.stringify(params, { encodeValuesOnly: true });
       },
     },
-  }).then(
-    (res) =>
-      res.data.data.map((m) => ({
-        id: m.attributes.user.data.id,
-        ...m.attributes.user.data.attributes,
-        domains: m.attributes.user.data.attributes.domains,
-        dimensions: m.attributes.user.data.attributes.dimensions,
-        reports: m.attributes.user.data.attributes.reports,
-        role: m.attributes.user.data.attributes.role,
-      })) ?? []
-  );
+  }).then((res) => res.data ?? []);
 };
